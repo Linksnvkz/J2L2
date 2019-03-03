42d7c5f14a898fd87e107721dc5438bda8dae268
Links <links_nvkz@mail.ru>
2019-03-03 19:14:01 +0300

    Выполнены все задачи, кроме *.

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,26 @@ public class Main {
         System.out.println("Введите количество столбцов массива:");
         col = scanner.nextInt();
         String[][] arr = new String[row][col];
-        newArray(arr, row, col);
+        newArray(arr, scanner, row, col);
     }
 
-    static void newArray (String[][] arr, int row, int col) {
+    static void newArray (String[][] arr, Scanner scanner, int row, int col) {
+        int sum = 0;
+        int[][] arrInt = new int[row][col];
         if (row != 4 || col != 4) throw new MyArraySizeException(row, col);
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr.length; j++){
+                System.out.println("Введите ячейку " + i + ":" + j);
+                arr[i][j] = scanner.next();
+                try{
+                    arrInt[i][j] = Integer.parseInt(arr[i][j]);
+                    sum+=arrInt[i][j];
+                } catch (Exception e) {
+                    throw new MyArrayDataException(i, j);
+                } finally {
+                    System.out.println("Сумма ячеек = " + sum);
+                }
+            }
+        }
     }
 }
