e9034f7a9737e2b6818dc5c9f69560443f1622d4
Linksnvkz <46680060+Linksnvkz@users.noreply.github.com>
2019-03-03 19:17:57 +0300

    Delete MyArrayDataException.java

--- src/MyArrayDataException.java
+++
@@ -1,9 +0,0 @@
-public class MyArrayDataException extends RuntimeException {
-    int row, col;
-
-    public MyArrayDataException(int row, int col){
-        super("Невозможно преобразовать в int в ячейке " + row + ":" + col);
-        this.row = row;
-        this.col = col;
-    }
-}
