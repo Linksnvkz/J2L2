c80a110d6fe0301001682ad3eb9066f3b34085c3
Links <links_nvkz@mail.ru>
2019-03-03 17:36:30 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+        System.out.println("Введите количество строк массива:");
+        row = scanner.nextInt();
+        System.out.println("Введите количество столбцов массива:");
+        col = scanner.nextInt();
+        String[][] arr = new String[row][col];
+        newArray(arr, row, col);
+    }
+
+    static void newArray (String[][] arr, int row, int col) {
+        if (row != 4 || col != 4) throw new MyArraySizeException(row, col);
     }
 }
